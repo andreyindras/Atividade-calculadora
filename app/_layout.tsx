70d693e981547aae1cad2c89366dc0320aedf067
andreyindras <andreyindras55@gmail.com>
2026-03-02 15:24:41 -0300

    implementação do layout da calculadora

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,101 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <View style={styles.containerCalculadora}>
+      <View style={styles.areaVisor}>
+        <Text></Text>
+      </View>
+
+      <View style={styles.areaBotoes}>     
+        <View style={styles.linhaBotoes}>
+          <TouchableOpacity style={styles.botaoLimparTudo}>
+            <Text>AC</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botao}>
+            <Text>√</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botao}>
+            <Text>/</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botao}>
+            <Text>*</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.linhaBotoes}>
+          <TouchableOpacity style={styles.botao}>
+            <Text>7</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botao}>
+            <Text>8</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botao}>
+            <Text>9</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botao}>
+            <Text>+</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.linhaBotoes}>
+          <TouchableOpacity style={styles.botao}>
+            <Text>4</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botao}>
+            <Text>5</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botao}>
+            <Text>6</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botao}>
+            <Text>-</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.linhaBotoes}>
+          <TouchableOpacity style={styles.botao}>
+            <Text>1</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botao}>
+            <Text>2</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botao}>
+            <Text>3</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botaoIgual}>
+            <Text>=</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.linhaBotoes}>
+          <TouchableOpacity style={styles.botaoZero}>
+            <Text>0</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botao}>
+            <Text>,</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+
+})
